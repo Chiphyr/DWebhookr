730a32c6ed26972e02718de3dec28beed871a326
Chiphyr <alexsssthereal@yahoo.com>
2019-07-02 22:43:35 +0100

    There we go

--- run-json.js
+++ run-json.js
@@ -9,11 +9,44 @@ const axios = require('axios');
 const config = require('./config.json');
 const chalk = require('chalk');
 
+function next(){
+    const questions = [{
+        type: 'input',
+        name: 'message',
+        message: "Message:\n",
+    }];
+    
+    inquirer.prompt(questions).then(answers => {
+        let url = config.url;
+        let username = config.username;
+        let message = answers['message'];
+    
+        let data = {
+            "username": username,
+            "content": message
+        };
+        
+        try{
+            axios({
+                method: 'post',
+                url: url,
+                data: data
+            });
+        } catch(e){
+            console.log('Agh! I encountered an error.\n\n\n')
+            console.log(e);
+            return;
+        };
+        console.log("That's been sent.");
+        next();
+    });
+};
+
 function run(){
     const questions = [{
         type: 'input',
         name: 'message',
-        message: "I'm using the config.json file. What's your message?\n",
+        message: "I'm using the config.json file. Message:\n",
     }];
 
     console.log(chalk.green.bold.underline("DWebhookr") + " by "  + chalk.bold("Chyphyr (Chiphyr ✨#0001)\n\n"));
@@ -40,7 +73,7 @@ function run(){
             return;
         };
         console.log("That's been sent.");
-        run();
+        next();
     });
 };
 
